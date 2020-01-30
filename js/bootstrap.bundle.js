9420ba428e47015e111e8f40963969cc5fba056b
tuananh9857 <49451970+tuananh9857@users.noreply.github.com>
2020-01-29 19:40:16 -0800

    Add files via upload

--- js/bootstrap.bundle.js
+++ js/bootstrap.bundle.js
@@ -3,6 +3,9 @@
   * Copyright 2011-2019 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
+  $(function () {
+  $('[data-toggle="popover"]').popover()
+})
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery')) :
   typeof define === 'function' && define.amd ? define(['exports', 'jquery'], factory) :
