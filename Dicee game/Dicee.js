c3129da359ccea17c2ebdfd4edfeabf768b9554a
tuananh9857 <49451970+tuananh9857@users.noreply.github.com>
2020-02-02 09:41:38 -0800

    Add files via upload

--- Dicee game/Dicee.js
+++ Dicee game/Dicee.js
@@ -1,21 +1,40 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+var i = 0;
 
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomDiceImage);
+document.querySelectorAll("button")[0].addEventListener("click", Draw);
 
-var randomNumber2 = Math.floor(Math.random()*6)+1;
-var randomDiceImage2 = "images/dice" + randomNumber2 +".png";
 
-var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src",randomDiceImage2);
 
-if (randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML = " Player 1 Win !!!!";
+function Draw() {
+
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+  var image1 = document.querySelectorAll("img")[0];
+  image1.setAttribute("src", randomDiceImage);
+
+  var image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src", randomDiceImage2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = " Player 1 Win !!!!";
+  } else if (randomNumber1 == randomNumber2) {
+    document.querySelector('h1').innerHTML = "Draw again";
+  } else {
+    document.querySelector("h1").innerHTML = " Player 2 Win !!!!";
+  }
+
+  buttonAnimation();
 }
-else if (randomNumber1 == randomNumber2){
-  document.querySelector('h1').innerHTML = "Draw again";
-}
-else {
-  document.querySelector("h1").innerHTML = " Player 2 Win !!!!";
+
+function buttonAnimation(button) {
+  var activeButton = document.querySelector(".drawbutton");
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+
 }
